42aa30d9e61f7b7ab4cc2d935999e54bfb18c8af
artemgon <artemhoncharuk123@gmail.com>
2024-11-03 15:29:15 +0200

    Creating the 1-st task.

--- funcs.h
+++ funcs.h
@@ -1,1 +1,40 @@
 #include "libs.h" 
+
+template<class T>
+T* joinFunc(T* p_array_1, T* p_array_2, int M, int N)
+{
+	T* j_array = new T[M + N];
+	for (int i = 0, j = 0; i < M + N; i++)
+	{
+		if (i < M)
+		{
+			j_array[i] = p_array_1[i];
+		}
+		else
+		{
+			j_array[i] = p_array_2[j];
+			j++;
+		}
+	}
+	return j_array;
+}
+
+template<class T>
+T* randomArr(int size)
+{
+	T* new_array = new T[size];
+	for (int i = 0; i < size; i++) {
+		new_array[i] = rand() % 10;
+	}
+	return new_array;
+}
+
+template<class T>
+void printArr(T* array, int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
+	srand(time(NULL));
+	int M, N;
+	cout << "Size of 1-st array:";
+	cin >> M;
+	cout << "Size of 2-nd array:";
+	cin >> N;
+	int* A = randomArr<int>(M);
+	int* B = randomArr<int>(N);
+	cout << "1-st array: " << endl;
+	printArr<int>(A, M);
+	cout << "2-nd array: " << endl;
+	printArr<int>(B, N);
+	int* C = joinFunc<int>(A, B, M, N);
+	printArr<int>(C, M + N);
+	delete[] A;
+	delete[] B;
+	delete[] C;
 	return 0;
 }
